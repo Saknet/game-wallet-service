2d07fa809212bed83d797231fa2b5e6dbbbd6057
Saknet <andreas.v.andra@gmail.com>
2025-12-27 12:57:12 +0200

    fix(test): Force WalletApplicationTests to use H2 profile to avoid Postgres connection error

--- src/test/kotlin/com/veikkaus/wallet/WalletApplicationTests.kt
+++ src/test/kotlin/com/veikkaus/wallet/WalletApplicationTests.kt
@@ -3,8 +3,10 @@ package com.veikkaus.wallet
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("test")
 class WalletApplicationTests {
 
     @Test
