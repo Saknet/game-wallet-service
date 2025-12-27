f7f4e9d12a526c274de7f613b12c84234ddea925
Saknet <andreas.v.andra@gmail.com>
2025-12-27 12:33:25 +0200

    fix(test): Force Integration Tests to use Postgres instead of H2

--- src/test/kotlin/com/veikkaus/wallet/WalletIntegrationTest.kt
+++ src/test/kotlin/com/veikkaus/wallet/WalletIntegrationTest.kt
@@ -3,12 +3,13 @@ package com.veikkaus.wallet
 import com.veikkaus.wallet.dto.TransactionRequest
 import com.veikkaus.wallet.model.Player
 import com.veikkaus.wallet.repository.PlayerRepository
-import com.veikkaus.wallet.repository.TransactionRepository // Import this
+import com.veikkaus.wallet.repository.TransactionRepository
 import com.veikkaus.wallet.service.WalletService
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.DynamicPropertyRegistry
 import org.springframework.test.context.DynamicPropertySource
@@ -23,6 +24,7 @@ import java.util.Collections
 
 @SpringBootTest
 @Testcontainers
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class WalletIntegrationTest {
 
     companion object {
